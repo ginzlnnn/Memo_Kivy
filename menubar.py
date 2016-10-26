65f3ebea3d2263b0b1feb9f91ec9a389826791ee
Jittawat <a5730042a@gmail.com>
2016-10-27 05:36:50 +0700

    Create 3 main files -Memo (root file) -Desk (Layout for tyoing note or see picture) -MenuBar (Layout for add text files) -StatusBar (Layout for show current Status like how many letter text file)

---
+++ menubar.py
@@ -0,0 +1,9 @@
+# File name: menubar.py
+import kivy
+kivy.require('1.9.0')
+
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MenuBar(BoxLayout):
+    pass
---
+++ statusbar.py
@@ -0,0 +1,9 @@
+# File name: statusbar.py
+import kivy
+kivy.require('1.9.0')
+
+from kivy.uix.boxlayout import BoxLayout
+
+
+class StatusBar(BoxLayout):
+    pass
