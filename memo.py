76dc9028ab2d48d0f8b88e3adb0c4711f5c5c0b9
Jittawat <a5730042a@gmail.com>
2016-10-29 00:05:13 +0700

    Still workking on Screen Manager

--- memo.py
+++ memo.py
@@ -5,14 +5,18 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
 
 Builder.load_file('menubar.kv')
 Builder.load_file('desk.kv')
 Builder.load_file('statusbar.kv')
 Builder.load_file('note.kv')
+loadScreen = Builder.load_file('screenmanager.kv')
+
 
 class Memo(AnchorLayout):
-    pass
+    def Build(self):
+        return loadScreen
 
 class MemoApp(App):
     def build(self):
