a3f0592f46bfcf953a415195c413eb8d3cec6695
Jittawat <a5730042a@gmail.com>
2016-10-29 01:38:31 +0700

    Make memo can switch between page form menu to note.

--- memo.py
+++ memo.py
@@ -1,26 +1,3 @@
-# File names: memo.py
-import kivy
-kivy.require('1.9.0')
-
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.properties import ObjectProperty
-
-Builder.load_file('menubar.kv')
-Builder.load_file('desk.kv')
-Builder.load_file('statusbar.kv')
-Builder.load_file('note.kv')
-loadScreen = Builder.load_file('screenmanager.kv')
-
-
-class Memo(AnchorLayout):
-    def Build(self):
-        return loadScreen
-
-class MemoApp(App):
-    def build(self):
-        return Memo()
-    
-if __name__ == "__main__":
-    MemoApp().run()
+from kivy.uix.screenmanager import Screen
+class Memo(Screen):
+    pass
--- note.py
+++ note.py
@@ -1,9 +1,11 @@
-# File name: menubar.py
+# File name: note.py
 import kivy
 kivy.require('1.9.0')
 
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class MenuBar(Screen):
+
+class Note(Screen):
     pass
+
