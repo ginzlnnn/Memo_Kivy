9dcd66c946afca6f857c3e9f1e502139b74b7dff
Jittawat <a5730042a@gmail.com>
2016-10-29 17:48:59 +0700

    Try to make TextInput using Popup() didn't work. Find new way?

--- writebox.py
+++ writebox.py
@@ -2,17 +2,12 @@
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-from kivy.app import App
+from kivy.uix.popup import Popup
+from kivy.uix import label
 
 
 class WriteBox(RelativeLayout):
-    def on_touch_up(self, touch):
-        b = BoxLayout()
-        textInput = TextInput(text='work')
-        self.add_widget(b)
-        b.add_widget(textInput)
-        print(textInput)
-        return b
-
+    def on_touch_down(self, touch):
+        print('call input text i hope?')
 if __name__ == "__main__":
     TutorialApp().run()
