88cdf898fcea6552cfe491ae11fe88b077aeea09
Jittawat <a5730042a@gmail.com>
2016-10-29 17:39:54 +0700

    Still try to make WriteBox Layout can inputText message.

--- writebox.py
+++ writebox.py
@@ -1,4 +1,18 @@
+
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+
+
 class WriteBox(RelativeLayout):
-    def on_touch_down(self, touch):
-        print('work')
+    def on_touch_up(self, touch):
+        b = BoxLayout()
+        textInput = TextInput(text='work')
+        self.add_widget(b)
+        b.add_widget(textInput)
+        print(textInput)
+        return b
+
+if __name__ == "__main__":
+    TutorialApp().run()
