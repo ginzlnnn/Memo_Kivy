ab7c9ef171f15c344ea3dab78d0d3e9c68659ec8
Thakdanai <Thakdanai@localhost>
2016-10-29 17:38:33 +0700

    open text file(show data in popup)

--- desk.py
+++ desk.py
@@ -3,11 +3,12 @@ import kivy
 kivy.require('1.9.0')
 
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.properties import StringProperty
 
 class Desk(BoxLayout):
+    fileSelect_name = StringProperty('')
     def selected(self, filename):
-        name = filename[0].rsplit('/',1)[1]
-        print('Filename : ' + name)
-        self.status_bar.showName = name
+        self.fileSelect_name = filename[0].rsplit('/',1)[1]
+        print('Filename : ' + self.fileSelect_name)
+        self.status_bar.showName = self.fileSelect_name
 
