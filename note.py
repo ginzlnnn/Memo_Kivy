a21c3722e6fd170dca2dba27c0d2272731814d7a
Jittawat <a5730042a@gmail.com>
2016-10-29 17:04:23 +0700

    Make Seperate file in note Layout into two 1) WriteBox 2) SaveBar

--- note.py
+++ note.py
@@ -1,11 +1,25 @@
 # File name: note.py
 import kivy
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
 kivy.require('1.9.0')
 
 from kivy.uix.screenmanager import ScreenManager, Screen
-
-
+from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
+Builder.load_file('savebar.kv')
+Builder.load_file('writebox.kv')
+Window.clearcolor = (1, 1, 1, 1)
 
 class Note(Screen):
-    pass
+    def writeNote(self):
+        b = BoxLayout()
+        t = TextInput(font_size = 50)
+        f = FloatLayout()
+        b.add_widget(f)
+        b.add_widget(t)
+        
+        return b
+        
 
