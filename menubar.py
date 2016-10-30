ce809b4af2e1840ff6b3ba2ac54464f1c490795b
Nitikarn <Nitikarn@localhost>
2016-10-30 21:01:31 +0700

    popup to confirm delete file

--- menubar.py
+++ menubar.py
@@ -15,7 +15,9 @@ class MenuBar(Screen):
         try:
             textfile = open(os.path.join(path, filename[0]))
             content = ShowText(text=textfile.read(),cancel=self.dismiss_popup)
-            self._popup = Popup(title=os.path.basename(filename[0]), content=content, size_hint=(None, None), size=(500,500))
+            self._popup = Popup(title=os.path.basename(filename[0]),
+                                content=content, size_hint=(None, None),
+                                size=(500,500))
             self._popup.open()
             print(self.desk.fileSelect_name)
         except IndexError:
@@ -24,15 +26,27 @@ class MenuBar(Screen):
     def dismiss_popup(self):
         self._popup.dismiss()
 
-    def delete(self):
-        try:
-            textfile = 'note/'+self.desk.fileSelect_name
-            os.remove(textfile)
-            self.desk.filechooser._update_files()
-            self.desk.filechooser.selection = []
-        except:
+    def show_delete(self):
+        sel = self.desk.filechooser.selection
+        if(sel == []):
             print('Please Select File!!')
+        else:
+            content = DeleteFile(delete = self.delete, no=self.dismiss_popup)
+            self._popup = Popup(title="Delete file", content=content,
+                            size_hint=(0.5, 0.2))
+            self._popup.open()
+            
+    def delete(self):
+        textfile = 'note/'+self.desk.fileSelect_name
+        os.remove(textfile)
+        self.desk.filechooser._update_files()
+        self.desk.filechooser.selection = []
+        self.dismiss_popup()
 
+class DeleteFile(RelativeLayout):
+    delete = ObjectProperty(None)
+    no = ObjectProperty(None)
+    
 class ShowText(RelativeLayout):
     text = ObjectProperty(None)
     cancel = ObjectProperty(None)
