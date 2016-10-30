c251b209025a64f30a611081191448279ba91e70
Thakdanai <Thakdanai@localhost>
2016-10-30 22:09:01 +0700

    Already Add Text File with Popup

--- menubar.py
+++ menubar.py
@@ -7,10 +7,16 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
 
 import os
 
 class MenuBar(Screen):
+    def add(self):
+        content = Add(cancel=self.dismiss_popup, save=self.save)
+        self._popup = Popup(title='Add Note', content=content, size_hint=(None, None), size=(500,500))
+        self._popup.open()
+
     def open(self, path, filename):
         try:
             textfile = open(os.path.join(path, filename[0]))
@@ -23,6 +29,8 @@ class MenuBar(Screen):
 
     def dismiss_popup(self):
         self._popup.dismiss()
+        self.desk.filechooser._update_files()
+        self.desk.filechooser.selection = []
 
     def delete(self):
         try:
@@ -33,8 +41,16 @@ class MenuBar(Screen):
         except:
             print('Please Select File!!')
 
+    def save(self, filename, content):
+        textfile = open(os.path.join('note/', filename), 'w')
+        textfile.write(content)
+
+        self.dismiss_popup()
+
 class ShowText(RelativeLayout):
     text = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
-
+class Add(RelativeLayout):
+    cancel = ObjectProperty(None)
+    save = ObjectProperty(None)
