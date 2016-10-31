ec799309b7c6c787b333ec4a814eb19f3859ef9b
Thakdanai <Thakdanai@localhost>
2016-10-31 10:45:11 +0700

    Already Edit textfile / Show Error Popup

--- menubar.py
+++ menubar.py
@@ -18,6 +18,18 @@ class MenuBar(Screen):
                             size_hint=(None, None), size=(500,500))
         self._popup.open()
 
+    def edit(self, path, filename):        
+        try:
+            textfile = open(os.path.join(path, filename[0]))
+            content = Add(cancel=self.dismiss_popup, save=self.save)
+            content.text_name.text = os.path.basename(filename[0])
+            content.text_content.text = textfile.read()
+            self._popup = Popup(title='Edit Note', content=content,
+                                size_hint=(None, None), size=(500,500))
+            self._popup.open()
+        except IndexError:
+            self.error_popup('Please Select File!!')
+
     def open(self, path, filename):
         try:
             textfile = open(os.path.join(path, filename[0]))
@@ -28,7 +40,7 @@ class MenuBar(Screen):
             self._popup.open()
             print(self.desk.fileSelect_name)
         except IndexError:
-            print('Please Select File!!')
+            self.error_popup('Please Select File!!')
 
     def dismiss_popup(self):
         self._popup.dismiss()
@@ -37,7 +49,7 @@ class MenuBar(Screen):
     def show_delete(self):
         sel = self.desk.filechooser.selection
         if(sel == []):
-            print('Please Select File!!')
+            self.error_popup('Please Select File!!')
         else:
             content = DeleteFile(delete = self.delete, no=self.dismiss_popup)
             self._popup = Popup(title="Delete file", content=content,
@@ -51,15 +63,24 @@ class MenuBar(Screen):
         self.dismiss_popup()
 
     def save(self, filename, content):
-        textfile = open(os.path.join('note/', filename), 'w')
-        textfile.write(content)
-        self.desk.filechooser.selection = []
-        self.dismiss_popup()
+        if(len(filename) == 0):
+            self.error_popup('Enter Filename!!')
+        else:
+            textfile = open(os.path.join('note/', filename), 'w')
+            textfile.write(content)
+            self.desk.filechooser.selection = []
+            self.dismiss_popup()
+
+    def error_popup(self, error_text):
+        content = ShowError(cancel=self.dismiss_popup)
+        content.text_error.text = error_text
+        self._popup = Popup(title='Error!', content=content, 
+                            size_hint=(None, None), size=(200,200))
+        self._popup.open()
 
 class DeleteFile(RelativeLayout):
     delete = ObjectProperty(None)
     no = ObjectProperty(None)
-    
 
 class ShowText(RelativeLayout):
     text = ObjectProperty(None)
@@ -68,3 +89,6 @@ class ShowText(RelativeLayout):
 class Add(RelativeLayout):
     cancel = ObjectProperty(None)
     save = ObjectProperty(None)
+
+class ShowError(RelativeLayout):
+    cancel = ObjectProperty(None)
