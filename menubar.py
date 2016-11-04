cadb73e6d70b11dc945a145f7f5cacdd9ee8364f
Jittawat <a5730042a@gmail.com>
2016-11-04 00:44:46 +0000

    Fix code for PEP8

--- menubar.py
+++ menubar.py
@@ -1,7 +1,3 @@
-# File name: menubar.py
-import kivy
-kivy.require('1.9.0')
-
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.relativelayout import RelativeLayout
@@ -9,29 +5,30 @@ from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
-
+# File name: menubar.py
+import kivy
 import os
-    
-class MenuBar(Screen):              # Class MenuBar. 
-    def dismiss_popup(self):        # Closed current popup and then update files.
-        self._popup.dismiss()       
+kivy.require('1.9.0')
+class MenuBar(Screen):  # Class MenuBar.
+    def dismiss_popup(self):  # Closed current popup and then update files.
+        self._popup.dismiss()
         self.desk.filechooser._update_files()
 
-    def add(self):                  # Create Add note popup that have two button save and cancel.
-        content = Add(cancel=self.dismiss_popup, save=self.save)                        # Content is Add class that have 2 button 
-                                                                                        # Cancel button call dissmis_popup method,save button call save method,
-        self._popup = Popup(title='Add Note', title_font='Waree', content=content,      # Set title name to 'Add Note' and make popup closed manually
-                            auto_dismiss=False, size_hint=(None, None), size=(500,500)) # Popup size is 500, 500
-        self._popup.open()                                                              # Open popup after create finish.
+    def add(self):  # Create Add note popup that have two button save and cancel.
+        content = Add(cancel=self.dismiss_popup, save=self.save)  # Content is Add class that have 2 button
+                                                                # Cancel button call dissmis_popup method,save button call save method,
+        self._popup = Popup(title='Add Note', title_font='Waree', content=content,  # Set title name to 'Add Note' and make popup closed manually
+                            auto_dismiss=False, size_hint=(None, None), size=(500, 500))  # Popup size is 500, 500
+        self._popup.open()  # Open popup after create finish.
 
-    def edit(self, path, filename):        
+    def edit(self, path, filename):
         try:
-            textfile = open(os.path.join(path, filename[0]), encoding = ('utf-8'))
-            content = Add(cancel=self.dismiss_popup, save=self.save)
-            content.text_name.text = os.path.basename(filename[0])
-            content.text_content.text = textfile.read()
-            self._popup = Popup(title='Edit Note', title_font='Waree', content=content,
-                                auto_dismiss=False, size_hint=(None, None), size=(500,500))
+            textfile= open(os.path.join(path, filename[0]), encoding= ('utf-8'))
+            content= Add(cancel= self.dismiss_popup, save= self.save)
+            content.text_name.text= os.path.basename(filename[0])
+            content.text_content.text= textfile.read()
+            self._popup= Popup(title= 'Edit Note', title_font= 'Waree', content= content,
+                                auto_dismiss= False, size_hint= (None, None), size= (500, 500))
             self._popup.open()
         except IndexError:
             self.error_popup('Please Select File!!')
@@ -43,7 +40,7 @@ class MenuBar(Screen):              # Class MenuBar.
         else:
             content = DeleteFile(delete = self.delete, no=self.dismiss_popup)
             self._popup = Popup(title="Delete file", content=content, auto_dismiss=False, 
-                            size_hint=(0.5, 0.2))
+                                                                    size_hint=(0.5, 0.2))
             self._popup.open()
             
     def delete(self):
@@ -66,7 +63,7 @@ class MenuBar(Screen):              # Class MenuBar.
         content = ShowError()
         content.text_error.text = error_text
         self.popup_error = Popup(title='Error!', content=content, auto_dismiss=False, 
-                            size_hint=(None, None), size=(200,200))
+                            size_hint=(None, None), size=(200, 200))
         content.cancel.bind(on_press=self.popup_error.dismiss)
 
         self.popup_error.open()
@@ -74,11 +71,11 @@ class MenuBar(Screen):              # Class MenuBar.
 class TextMenu(MenuBar):
     def open(self, path, filename):
         try:
-            textfile = open(os.path.join(path, filename[0]), encoding= ('utf-8'))
-            content = ShowText(text=textfile.read(),cancel=self.dismiss_popup)
-            self._popup = Popup(title=os.path.basename(filename[0]), title_font='Waree',
-                                auto_dismiss=False, content=content, size_hint=(None, None),
-                                size=(500,500))
+            textfile= open(os.path.join(path, filename[0]), encoding= ('utf-8'))
+            content= ShowText(text= textfile.read(), cancel= self.dismiss_popup)
+            self._popup= Popup(title= os.path.basename(filename[0]), title_font= 'Waree',
+                                auto_dismiss= False, content= content, size_hint= (None, None),
+                                size= (500,500))
             self._popup.open()
         except IndexError:
             self.error_popup('Please Select File!!')
@@ -86,19 +83,19 @@ class TextMenu(MenuBar):
 class PictureMenu(MenuBar):
     def open(self):
         try:
-            content = ShowPicture(source = self.desk.get_path(),cancel=self.dismiss_popup)
+            content= ShowPicture(source= self.desk.get_path(),cancel= self.dismiss_popup)
             self._popup = Popup(title=os.path.basename(self.desk.get_path()), title_font='Waree',
                                 auto_dismiss=False, content=content, size_hint=(None, None),
-                                size=(500,500))
+                                size=(500, 500))
             self._popup.open()
         except IndexError:
             self.error_popup('Please Select File!!')
             
     def import_picture(self):
-        content = ImportPicture(load = self.load(),cancel=self.dismiss_popup)
-        self._popup = Popup(title='Import Picture', title_font='Waree',
-                            auto_dismiss=False, content=content, size_hint=(None, None),
-                            size=(500,500))
+        content= ImportPicture(load= self.load(),cancel= self.dismiss_popup)
+        self._popup= Popup(title= 'Import Picture', title_font= 'Waree',
+                            auto_dismiss= False, content= content, size_hint= (None, None),
+                            size= (500, 500))
         self._popup.open()
 
     def load(self):
