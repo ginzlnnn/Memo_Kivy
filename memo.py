2f1dcf26dad8b1ad092051721f328858819c3754
Nitikarn <Nitikarn@localhost>
2016-11-06 17:04:26 +0700

    edit gui use only one screen

--- memo.py
+++ memo.py
@@ -1,3 +1,19 @@
-from kivy.uix.screenmanager import Screen
-class Memo(Screen):
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
+
+Builder.load_file('memo.kv')      #load memo.kv File
+
+class Memo(AnchorLayout):
     pass
+
+class MemoApp(App):
+    def build(self):
+        return Memo()
+    
+if __name__ == "__main__":
+    MemoApp().run()
