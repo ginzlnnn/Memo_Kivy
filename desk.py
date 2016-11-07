2e2e3da5e72ca865b404bb9c5d3eba03d77a7839
Nitikarn <Nitikarn@localhost>
2016-11-08 01:07:27 +0700

    Fix code for pep8

--- desk.py
+++ desk.py
@@ -1,16 +1,18 @@
 # File name: desk.py
 import kivy
-kivy.require('1.9.0')
 
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.properties import StringProperty
 
 import os
- 
-class Desk(BoxLayout):                       # Desk class is a space for user to see files and select it
-    fileSelect_name = StringProperty('')    # FilesSelect_name change when select another files
-    def selected(self, filename):           
+
+
+# Desk class is a space for user to see files and select it
+class Desk(BoxLayout):
+    # FilesSelect_name change when select another files
+    fileSelect_name = StringProperty('')
+
+    def selected(self, filename):
         try:
             self.fileSelect_name = os.path.basename(filename[0])
             print('Filename : ' + self.fileSelect_name)
@@ -18,10 +20,11 @@ class Desk(BoxLayout):                       # Desk class is a space for user to
             self.fileSelect_name = ''
         self.status_bar.showName = self.fileSelect_name
 
-    def file_count(self, path):                   # Count files in floder 
-        list = os.listdir(path = path)    # list files in floder 
+    def file_count(self, path):                   # Count files in floder
+        list = os.listdir(path=path)    # list files in floder
         number = len(list)
-        self.status_bar.total = number      # Value total in status_bar equal number
+        # Value total in status_bar equal number
+        self.status_bar.total = number
 
     def get_path(self):
         return self.filechooser.selection[0]
